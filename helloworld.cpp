b41f9e87f45f61b40552d9018759f4095b845d12
Colton Wedell <wedell@chapman.edu>
2019-09-05 05:18:02 +0000

    Finalized helloworld program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,14 @@
+// Colton Wedell
+// Assignment Hello World
+// CPSC350
+// Rene German
+// 9/5/2019
+
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-cout << "foobar" << endl;
-return 1;
+cout << "hello world" << endl;
+return 0;
 }
